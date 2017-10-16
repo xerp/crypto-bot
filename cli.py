9604eaea70b3410835a3f4598215b80dd0fe1fa7
Santiago de Pedro <santiago.pedro23@gmail.com>
2017-10-16 12:03:05 -0400

    changes on cli file

--- cli.py
+++ cli.py
@@ -5,6 +5,10 @@ from exchanges import loader
 CLI_COMMANDS = EXCHANGE_COMMANDS
 CLI_COMMANDS['exchanges'] = map, lambda e: e.name, loader.get_available_exchanges
 
+CLI_FUNCTIONS = {}
+
+
+# ------------ CLI Utils Functions BEGIN ------------------
 
 def cli_exit(message=None, exit_code=0):
     if message:
@@ -13,7 +17,7 @@ def cli_exit(message=None, exit_code=0):
     exit(exit_code)
 
 
-def get_cli_option(*args):
+def get_cli_option(args):
     try:
         return args[1]
     except IndexError:
@@ -27,31 +31,20 @@ def get_cli_function(option):
         cli_exit('Option is not valid')
 
 
-def print_exchanges(cli_function):
-    try:
-        exchanges = cli_function[0](cli_function[1], cli_function[3])
-        print(','.join(exchanges))
-    except IndexError:
-        cli_exit('exchanges command value must be a tuple structured as [function,lambda expression, list]')
-
-
 def print_exchange_values(**kwargs):
     for parameter, parameter_value in kwargs.items():
         print(f'{parameter}::{parameter_value}')
 
 
-def print_exchange_option(option, exchange_function, *args):
+def print_exchange_option(option, exchange_function, args):
     try:
         exchange_name = args[1]
         exchange = loader.get_exchange_by_name(exchange_name)
         if not exchange:
             raise ExchangeError(f'{exchange_name} exchange is not available')
 
-        coin = args[2]
-
-        if option == 'info':
-            coin_info = exchange_function(coin)
-            print_exchange_values(coin_info)
+        exchange_args = args[2::]
+        CLI_FUNCTIONS[option](exchange_function, exchange_args)
 
         cli_exit()
 
@@ -61,6 +54,33 @@ def print_exchange_option(option, exchange_function, *args):
         else:
             cli_exit('Missing arguments for this option')
 
+
+# ------------ CLI Utils Functions END ------------------
+
+
+# ------------ CLI Functions BEGIN ------------------
+
+def print_exchanges(cli_function):
+    try:
+        exchanges = cli_function[0](cli_function[1], cli_function[3])
+        print(','.join(exchanges))
+    except IndexError:
+        cli_exit('exchanges command value must be a tuple structured as [function,lambda expression, list]')
+
+
+def process_info(exchange_function, exchange_args):
+    coin = exchange_args[1]
+
+    coin_info = exchange_function(coin)
+    print_exchange_values(coin_info)
+
+
+# ------------ CLI Functions END ------------------
+
+
+CLI_FUNCTIONS = {
+    'info': process_info
+}
 
 if __name__ == '__main__':
 
