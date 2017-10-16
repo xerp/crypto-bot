dbf0e8fa9d4068e4bcfe7d063850a23618aa715e
Santiago de Pedro <santiago.pedro23@gmail.com>
2017-10-16 15:13:16 -0400

    changes on cli file

--- cli.py
+++ cli.py
@@ -2,13 +2,24 @@ import sys
 from exchange import EXCHANGE_COMMANDS, ExchangeError
 from exchanges import loader
 
-CLI_COMMANDS = EXCHANGE_COMMANDS
-CLI_COMMANDS['exchanges'] = map, lambda e: e.name, loader.get_available_exchanges
-
 CLI_FUNCTIONS = {}
 
 
-# ------------ CLI Utils Functions BEGIN ------------------
+# #################################################
+# ------------ CLI Utils Functions BEGIN ----------
+# #################################################
+
+
+def cli_function(name):
+    def wrapper(func):
+        function_name = func.__name__
+
+        CLI_FUNCTIONS[name] = function_name
+
+        return func
+
+    return wrapper
+
 
 def cli_exit(message=None, exit_code=0):
     if message:
@@ -24,9 +35,9 @@ def get_cli_option(args):
         cli_exit('no option to perform')
 
 
-def get_cli_function(option):
+def get_exchange_function(option):
     try:
-        return CLI_COMMANDS[option]
+        return EXCHANGE_COMMANDS[option]
     except KeyError:
         cli_exit('Option is not valid')
 
@@ -55,19 +66,23 @@ def print_exchange_option(option, exchange_function, args):
             cli_exit('Missing arguments for this option')
 
 
-# ------------ CLI Utils Functions END ------------------
+# #################################################
+# ------------ CLI Utils Functions END ------------
+# #################################################
 
+# #################################################
+# ------------ CLI Functions BEGIN ----------------
+# #################################################
 
-# ------------ CLI Functions BEGIN ------------------
-
-def print_exchanges(cli_function):
+def print_exchanges():
     try:
-        exchanges = cli_function[0](cli_function[1], cli_function[3])
+        exchanges = map(lambda e: e.name, loader.get_available_exchanges())
         print(','.join(exchanges))
     except IndexError:
         cli_exit('exchanges command value must be a tuple structured as [function,lambda expression, list]')
 
 
+@cli_function('info')
 def process_info(exchange_function, exchange_args):
     coin = exchange_args[1]
 
@@ -75,19 +90,16 @@ def process_info(exchange_function, exchange_args):
     print_exchange_values(coin_info)
 
 
+# #################################################
 # ------------ CLI Functions END ------------------
-
-
-CLI_FUNCTIONS = {
-    'info': process_info
-}
+# #################################################
 
 if __name__ == '__main__':
 
     cli_option = get_cli_option(sys.argv)
-    cli_function = get_cli_function(cli_option)
+    exchange_function = get_exchange_function(cli_option)
 
     if cli_option == 'exchanges':
-        print_exchanges(cli_function)
+        print_exchanges()
     else:
-        print_exchange_option(cli_option, cli_function, sys.argv)
+        print_exchange_option(cli_option, exchange_function, sys.argv)
