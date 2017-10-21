bc6f0924beb996d0e9cc97d95429b22b89e894c3
Santiago de Pedro <santiago.pedro23@gmail.com>
2017-10-21 16:29:25 -0400

    Testing poloniex logic

--- tests/poloniex_test.py
+++ tests/poloniex_test.py
@@ -12,6 +12,7 @@ class PoloniexTestCast(unittest.TestCase):
         self.poloniex = loader.get_exchange_by_name('poloniex')
         self.poloniex.connect_to_api(api_keys)
 
+    @unittest.skip("testing skipping")
     def test_get_coin_info(self):
         coins = ['btc', 'eth']
 
@@ -19,24 +20,28 @@ class PoloniexTestCast(unittest.TestCase):
 
         self.assertTrue(info, 'coin info failing')
 
+    @unittest.skip("testing skipping")
     def test_get_balance(self):
         coin = None
         balances = self.poloniex.get_balance(coin)
 
         self.assertTrue(balances, 'coin balance failing')
 
+    @unittest.skip("testing skipping")
     def test_get_open_orders(self):
         coins = None
         open_orders = self.poloniex.get_open_orders(coins)
 
         self.assertTrue(open_orders, 'open orders failing')
 
+    @unittest.skip("testing skipping")
     def test_get_order_history(self):
         coins = ['btc', 'eth']
         order_history = self.poloniex.get_order_history(coins)
 
         self.assertTrue(order_history, 'order history failing')
 
+    @unittest.skip("testing skipping")
     def test_get_trade_history(self):
         coins = ['btc', 'eth']
         start_date = '8/10/2017'
@@ -45,6 +50,7 @@ class PoloniexTestCast(unittest.TestCase):
 
         self.assertTrue(trade_history, 'trade history failing')
 
+    @unittest.skip("testing skipping")
     def test_get_chart_info(self):
         coins = ['btc', 'eth']
         start_date = '8/10/2017'
@@ -53,30 +59,31 @@ class PoloniexTestCast(unittest.TestCase):
 
         self.assertTrue(chart_info, 'chart info failing')
 
+    @unittest.skip("testing skipping")
     def test_buy(self):
         coins = ['btc', 'sc']
-        quantity = 1
-        price = 0
+        quantity = 200
+        price = 0.00000060
 
         result = self.poloniex.buy(coins, quantity, price)
-        print('buy', result)
+        print(result['orderNumber'])
 
-        self.assertTrue(result['success'], 'buying coins failing')
+        self.assertTrue(result['orderNumber'], 'buying coins failing')
 
     @unittest.skip("testing skipping")
     def test_sell(self):
         coins = ['btc', 'sc']
-        quantity = 1
-        price = 0
+        quantity = 199.5
+        price = 0.00000060
 
         result = self.poloniex.sell(coins, quantity, price)
-        print('sell', result)
+        print(result['orderNumber'])
 
-        self.assertTrue(result['success'], 'selling coins failing')
+        self.assertTrue(result['orderNumber'], 'selling coins failing')
 
     @unittest.skip("testing skipping")
     def test_cancel_order(self):
-        order_number = 1
+        order_number = 15009693432
 
         result = self.poloniex.cancel_order(order_number)
 
